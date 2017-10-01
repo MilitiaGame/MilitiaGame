00c9e4d33a823ffe16c7489286828566e7af4cb3
Harshdeep Singh <harshdeep@Harshdeeps-MacBook-Air.local>
2017-10-01 16:00:23 +0100

    add the initial structure of the game, initial Unet

---
+++ MultiPlayerFPS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+	[SerializeField]
+	private float speed = 5f;
+	[SerializeField]
+	private float lookSensitivity = 3;
+
+	private PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+		motor = GetComponent<PlayerMotor>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Calculate movement velocity as a 3d vector so that we can move the motor in that space!
+		// No processing done by Unity so better choose raw axis, and no smoothing to be done
+		// For movement along axes, along [-1, 1]
+		float xMovement = Input.GetAxisRaw("Horizontal");
+		float zMovement = Input.GetAxisRaw("Vertical");
+
+		// Movement backwards and forwards and upwards and downwards by updating the Vectors along those
+		Vector3 moveHorizontal = transform.right * xMovement;
+		Vector3 moveVertical = transform.forward * zMovement;
+
+		Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+		//Apply the movement to a motor
+		motor.Move(velocity);
+
+		//Calculate rotation as a 3D vector - set up using the Unity Input
+		// Want to move only along left right, up down can be done using camera
+		float yRotation = Input.GetAxisRaw("Mouse X");
+		Vector3 rotation = new Vector3(0f, yRotation, 0f) * lookSensitivity; // sort of scales
+		//Apply rotation
+		motor.Rotate(rotation);
+
+		//Calculate camera rotation as a 3D vector - set up using the Unity Input
+		float cameraRotation = Input.GetAxisRaw("Mouse Y");
+		Vector3 cRotation = new Vector3(cameraRotation, 0f, 0f) * lookSensitivity;
+		//Apply camera rotation
+		motor.RotateCamera(cRotation);
+	}
+}
---
+++ MultiPlayerFPS/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+	[SerializeField]
+	private Camera cam;
+
+	// Initially velocity,rotation will be zero
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 rotation = Vector3.zero;
+	private Vector3 rotationCamera = Vector3.zero;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Gets a movement vector from PlayerController
+	public void Move(Vector3 _velocity) {
+		velocity = _velocity;
+	}
+
+	// Gets a rotation vector from PlayerController
+	public void Rotate(Vector3 _rotation) {
+		rotation = _rotation;
+	}
+
+	// Gets a rotation camera vector from PlayerController
+	public void RotateCamera(Vector3 _rotationc) {
+		rotationCamera = _rotationc;
+	}
+
+	// Run or do movement afer an iteration
+	public void FixedUpdate() {
+		PerformMovement();
+		PerformRotation();
+	}
+
+	//Perform movement based on velocity variable
+	public void PerformMovement() {
+		if(velocity != Vector3.zero) {
+			// will stop the movement if there is an obstacle in the way
+			// Distance = speed * time
+			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+		}
+	}
+
+	//Perform rotation based on rotation variable
+	public void PerformRotation() {
+		if(rotation != Vector3.zero) {
+			// will stop the rotation if there is an obstacle in the way
+			// Quaternion is basically a different way to represent the angle rotation in space
+			rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+		}
+		if (cam != null) {
+			cam.transform.Rotate(-rotationCamera);
+		}
+	}
+}
